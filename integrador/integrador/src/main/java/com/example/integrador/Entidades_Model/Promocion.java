f5f5a57d3ccdf7f6a287b9667498a4bd5ff9079f
Sebastiandiazx24 <sebastianbernadiaz@gmail.com>
2024-11-19 13:29:35 -0500

    Entidad para Promociones.

---
+++ integrador/integrador/src/main/java/com/example/integrador/Entidades_Model/Promocion.java
@@ -0,0 +1,97 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.integrador.Entidades_Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDate;
+
+@Entity
+public class Promocion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long promocionId;
+    private String nombre;
+    private String descripcion;
+    private Double precio;
+    private int stock;
+    private String imagenUrl;
+    private LocalDate fechaInicio;
+    private LocalDate fechaFin;
+
+    // Getters and Setters
+    public Long getPromocionId() {
+        return promocionId;
+    }
+
+    public void setPromocionId(Long promocionId) {
+        this.promocionId = promocionId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getImagenUrl() {
+        return imagenUrl;
+    }
+
+    public void setImagenUrl(String imagenUrl) {
+        this.imagenUrl = imagenUrl;
+    }
+
+    public LocalDate getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDate fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDate getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDate fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+}
