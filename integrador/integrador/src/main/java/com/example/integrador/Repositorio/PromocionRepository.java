c0f0f7b7bb8f798a840acafa0d2e4af9a67ed083
Sebastiandiazx24 <sebastianbernadiaz@gmail.com>
2024-11-30 09:55:57 -0500

    PromocionRepository

--- integrador/integrador/src/main/java/com/example/integrador/Repositorio/PromocionRepository.java
+++ integrador/integrador/src/main/java/com/example/integrador/Repositorio/PromocionRepository.java
@@ -1,16 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package com.example.integrador.Repositorio;
 
 import com.example.integrador.Entidades_Model.Promocion;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
-
-public interface PromocionRepository extends JpaRepository<Promocion, Long> {
-    List<Promocion> findByFechaInicioBeforeAndFechaFinAfter(LocalDate now1, LocalDate now2);
+@Repository
+public interface PromocionRepository extends JpaRepository<Promocion, Integer> {
 }
-
